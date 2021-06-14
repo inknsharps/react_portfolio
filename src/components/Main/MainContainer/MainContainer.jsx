92f885804d720ede89c1d9e32f3639d92cb0848d
inknsharps <nchen25@gmail.com>
2021-06-14 16:44:06 -0400

    add routing back to home page, change circle generation

--- src/components/Main/MainContainer/MainContainer.jsx
+++ src/components/Main/MainContainer/MainContainer.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Hero from "../../Hero/HeroContainer/HeroContainer";
+import HeroContainer from "../../Hero/HeroContainer/HeroContainer";
 import "./MainContainer.css";
 
 const MainContainer = () => {
 	return (
 		<main className="MainContainer">
-			<Hero />
+			<HeroContainer />
 		</main>
 	)
 };
--- src/components/Navbar/NavbarContainer/NavbarContainer.jsx
+++ src/components/Navbar/NavbarContainer/NavbarContainer.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import NavbarLi from "../NavbarLi/NavbarLi";
 import "./NavbarContainer.css";
 
 const NavbarContainer = () => {
 	return (
 		<nav className="NavbarContainer flex justify-between p-5">
-			<h1 className="bg-gradient-to-l from-green-700 to-indigo-700 bg-clip-text text-transparent text-3xl">Neil Chen</h1>
+			<NavLink to="/">
+				<h1 className="bg-gradient-to-l from-green-700 to-indigo-700 bg-clip-text text-transparent text-3xl">Neil Chen</h1>
+			</NavLink>
 			<ul className="grid grid-cols-3 gap-x-5">
 				<NavbarLi liText="About" liLink="about" />
 				<NavbarLi liText="Projects" liLink="projects" />

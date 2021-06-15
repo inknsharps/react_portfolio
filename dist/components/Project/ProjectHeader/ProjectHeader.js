0ebd63a1b4dda38a94b8ea75de29903c72983f67
inknsharps <nchen25@gmail.com>
2021-06-15 18:07:57 -0400

    Updates

---
+++ dist/components/Project/ProjectHeader/ProjectHeader.js
@@ -0,0 +1,8 @@
+import React from "../../../../_snowpack/pkg/react.js";
+import "./ProjectHeader.css";
+const ProjectHeader = () => {
+  return /* @__PURE__ */ React.createElement("div", {
+    className: "ProjectHeader"
+  });
+};
+export default ProjectHeader;

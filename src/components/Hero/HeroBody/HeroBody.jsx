335bcc8cfd5ec5ad8d889b5dbf491918a53b9b1c
inknsharps <nchen25@gmail.com>
2021-06-15 15:32:18 -0400

    add medium size responsiveness to main page

--- src/components/Hero/HeroBody/HeroBody.jsx
+++ src/components/Hero/HeroBody/HeroBody.jsx
@@ -3,7 +3,7 @@ import "./HeroBody.css";
 
 const HeroBody = ({ alignItems, text, textAlign }) => {
 	return (
-		<div className={`HeroBody flex ${ alignItems } row-span-2`}>
+		<div className={`HeroBody flex ${ alignItems } md:row-span-2`}>
 			<h3 className={`p-2 xl:p-5 xl:mx-16 bg-gradient-to-r from-green-800 via-indigo-800 to-red-800 bg-clip-text text-transparent text-5xl ${ textAlign }`}>{ text }</h3>
 		</div>
 	)
--- src/components/Hero/HeroHeader/HeroHeader.jsx
+++ src/components/Hero/HeroHeader/HeroHeader.jsx
@@ -3,7 +3,7 @@ import "./HeroHeader.css";
 
 const HeroHeader = () => {	
 	return (
-		<div className="HeroHeader xl:row-span-2">
+		<div className="HeroHeader md:col-span-1 md:row-span-2">
 			<h2 className="p-2 xl:p-6 xl:mx-16 bg-gradient-to-r from-green-500 via-indigo-500 to-red-500 bg-clip-text text-transparent text-7xl xl:text-8xl text-left">Hello, my name is Neil Chen.</h2>
 		</div>
 	)

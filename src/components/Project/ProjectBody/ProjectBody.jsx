0a370e6bb22485219f1908af57deca1936564d3a
inknsharps <nchen25@gmail.com>
2021-06-15 15:44:53 -0400

    update responsiveness for project page

--- src/components/Project/ProjectBody/ProjectBody.jsx
+++ src/components/Project/ProjectBody/ProjectBody.jsx
@@ -5,8 +5,8 @@ import "./ProjectBody.css";
 const ProjectBody = ({ projectName, projectDesc, githubLink, deployLink }) => {
 	return (
 		<div className="ProjectBody col-span-2 flex flex-col justify-evenly m-5 bg-white bg-gradient-to-r from-green-800 via-indigo-800 to-blue-800 bg-clip-text text-transparent rounded-full">
-			<h3 className="p-5 mx-3 xl:mx-16 text-3xl xl:text-5xl">{ projectName }</h3>
-			<p className="p-5 xl:mx-16 text-lg xl:text-3xl">{ projectDesc }</p>
+			<h3 className="p-5 mx-3 xl:mx-16 text-3xl md:text-4xl xl:text-5xl">{ projectName }</h3>
+			<p className="p-5 xl:mx-16 text-lg md:text-2xl xl:text-3xl">{ projectDesc }</p>
 			<ProjectLink githubLink={ githubLink } deployLink={ deployLink } />
 		</div>
 	)

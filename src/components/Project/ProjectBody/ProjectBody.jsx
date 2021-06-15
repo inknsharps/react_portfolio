ffe9ea863e43a2ba536e596786721834237bb58b
inknsharps <nchen25@gmail.com>
2021-06-15 00:54:14 -0400

    fix icon styling bug in projectbody component

--- src/components/Project/ProjectBody/ProjectBody.jsx
+++ src/components/Project/ProjectBody/ProjectBody.jsx
@@ -4,9 +4,9 @@ import "./ProjectBody.css";
 
 const ProjectBody = ({ projectName, projectDesc, githubLink, deployLink }) => {
 	return (
-		<div className="ProjectBody col-span-2 flex flex-col justify-evenly m-5 bg-white from-white via-gray-100 to-white rounded-full">
-			<h3 className="p-5 mx-16 bg-gradient-to-r from-green-800 via-indigo-800 to-blue-800 bg-clip-text text-transparent text-5xl">{ projectName }</h3>
-			<p className="p-5 mx-16 bg-gradient-to-r from-green-800 via-indigo-800 to-blue-800 bg-clip-text text-transparent text-3xl">{ projectDesc }</p>
+		<div className="ProjectBody col-span-2 flex flex-col justify-evenly m-5 bg-white bg-gradient-to-r from-green-800 via-indigo-800 to-blue-800 bg-clip-text text-transparent rounded-full">
+			<h3 className="p-5 mx-16 text-5xl">{ projectName }</h3>
+			<p className="p-5 mx-16 text-3xl">{ projectDesc }</p>
 			<ProjectLink githubLink={ githubLink } deployLink={ deployLink } />
 		</div>
 	)
